634125fbb77e513534eb62af23e805b56d475b3c
Ichinose Shogo <ichinose@stn.nagaokaut.ac.jp>
2011-10-14 21:39:55 +0900

    Rectの処理を追加

---
+++ svg.py
@@ -0,0 +1,172 @@
+# -*- coding:utf-8 -*-
+
+import xml.sax
+import xml.sax.handler
+import sys
+import re
+
+class Parser:
+	def __init__(self):
+		self.__parser = xml.sax.make_parser()
+		self.__handler = SVGXMLHandler()
+		self.__parser.setContentHandler(self.__handler)
+		self.__parser.setFeature(xml.sax.handler.feature_external_ges, False)
+	
+	def parse(self, data):
+		self.__parser.parse(data)
+		return self.__handler.getSVG()
+	
+
+class SVGXMLHandler(xml.sax.handler.ContentHandler):
+	def __init__(self):
+		self.__container = None
+		self.__svg = None
+		
+	def startElement(self, name, attrs):
+		if name=="svg":
+			self.__container = SVG(attrs, self.__container)
+			self.__svg = self.__container
+		elif name=="rect":
+			self.__container.append(Rect(attrs))
+			
+	def endElement(self, name):
+		pass
+		
+	def getSVG(self):
+		return self.__svg
+
+#SVGの要素を表すクラス
+class Element:
+	def __init__(self, attrs, parent=None):
+		self.__parent = parent
+		self.id = attrs.get("id", "")
+	
+	def callHandler(self, handler):
+		pass
+	
+	@apply
+	def parrent():
+		def get(self):
+			return self.__parent
+
+#他のSVG要素を格納できるコンテナ
+class Container(Element,list):
+	def __init__(self, attrs, parent=None):
+		Element.__init__(self, attrs, parent)
+		list.__init__(self)
+		
+	def append(self, x):
+		list.append(self, x)
+		x._Element__parent = self
+		
+	def extend(self, L):
+		list.extend(self, L)
+		for x in L:
+			x._Element__parent = self
+	
+	def insert(self, i, x):
+		list.insert(self, i, x)
+		x._Element__parent = self
+
+	def remove(self, x):
+		list.remove(self, x)
+		x._Element__parent = None
+	
+	def pop(self, i=-1):
+		list.pop(self, i)
+		x._Element__parent = None
+
+#SVG画像
+class SVG(Container):
+	def __init__(self, attrs, parent=None):
+		Element.__init__(self, attrs, parent)
+		self.x = Length(attrs.get("x", "0"))
+		self.y = Length(attrs.get("y", "0"))
+		self.width = Length(attrs.get("width", "0"))
+		self.height = Length(attrs.get("height", "0"))
+		
+	def callHandler(self, handler):
+		handler.svg(self)
+
+#長方形
+class Rect(Element):
+	def __init__(self, attrs, parent=None):
+		Element.__init__(self, attrs, parent)
+		self.x = Length(attrs.get("x", "0"))
+		self.y = Length(attrs.get("y", "0"))
+		self.width = Length(attrs.get("width", "0"))
+		self.height = Length(attrs.get("height", "0"))
+		self.rx = Length(attrs["rx"]) if attrs.has_key("rx") else None
+		self.ry = Length(attrs["ry"]) if attrs.has_key("ry") else None
+		self.style = Style(attrs.get("style", ""))
+		
+	def callHandler(self, handler):
+		handler.rect(self)
+
+
+#SVG内での長さを表すクラス
+class Length:
+	__length_re = re.compile(r"(?P<length>[+\-0-9.]*)(?P<unit>[%a-z]*)")
+	__px_per_unit = {
+		"px": 1.0,
+		"in": 90.0,
+		"mm": 90.0/25.4,
+		"cm": 90.0/2.54,
+	}
+	
+	def __init__(self, length, unit = None):
+		if unit:
+			self.__length = float(length)
+			self.__unit = unit
+		else:
+			m = Length.__length_re.match(str(length))
+			if not m: raise
+			self.__length = float(m.group('length'))
+			self.__unit = m.group('unit') or "px"
+	
+	@property
+	def px(self):
+		return self.__length * Length.__px_per_unit[self.__unit]
+	
+	def __str__(self):
+		return str(self.__length) + self.__unit
+	
+	def __add__(a, b):
+		return Length(a.px + b.px, "px")
+	
+	def __sub__(a, b):
+		return Length(a.px - b.px, "px")
+	
+	def __mul__(a, b):
+		return Length(a.__length * b, a.__unit)
+	
+	def __rmul__(a, b):
+		return Length(a.__length * b, a.__unit)
+	
+	def __div__(a, b):
+		return Length(a.__length / b, a.__unit)
+
+#スタイル
+class Style(dict):
+	def __init__(self, style=""):
+		for item in style.split(";"):
+			a = item.split(":")
+			if len(a)<2: continue
+			self[a[0]] = a[1]
+
+class SVGHandler:
+	def svg(self, x):
+		for a in x:
+			a.callHandler(self)
+			
+	def rect(self, x):
+		pass
+
+def main():
+	filename = sys.argv[1]
+	p = Parser()
+	p.parse(open(filename, "r"))
+	return
+
+if __name__=="__main__":
+	main()
---
+++ svg2css.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import sys
+import svg
+
+class CSSWriter(svg.SVGHandler):
+	def __init__(self, name):
+		self.__name = name
+		self.__html = open(name + ".html", "w")
+		self.__css = open(name + ".css", "w")
+		self.__id = 0
+		
+	def newName(self):
+		self.__id = self.__id + 1
+		return "id%04d" % self.__id
+		
+	def svg(self, x):
+		self.__html.write("""<!DOCTYPE html> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="content-script-type" content="text/javascript" /> 
+<meta http-equiv="content-style-type" content="text/css" /> 
+<link rel="stylesheet" href="./%s.css">
+</head>
+<body>
+<div class="svg">\n""" % self.__name)
+		self.__css.write('@charset "utf-8"\n\n')
+		self.__css.write("div.svg{width:%s;height: %s;position: absolute;}\n" % (str(x.width), str(x.height)))
+		svg.SVGHandler.svg(self, x)
+		self.__html.write("""</div>\n</body></html>\n""")
+		
+	def rect(self, x):
+		name = self.newName()
+		css = {}
+		stroke = svg.Length(0)
+		
+		#ストロークの描画
+		if "stroke" in x.style and x.style["stroke"] != 'none':
+			css["border-style"] = "solid"
+			css["border-color"] =  x.style["stroke"]
+			stroke = svg.Length(x.style.get("stroke-width",1))
+			css["border-width"] = str(stroke)
+		
+		#位置と大きさの設定
+		css["position"] = "absolute"
+		css["left"] = str(x.x - stroke/2)
+		css["top"] = str(x.y - stroke/2)
+		css["width"] = str(x.width - stroke)
+		css["height"] = str(x.height - stroke)
+		
+		#角を丸める
+		if x.rx and x.ry:
+			css["border-radius"] = "%s/%s" % (str(x.rx+stroke/2), str(x.ry+stroke/2))
+		elif x.rx:
+			css["border-radius"] = str(x.rx+stroke/2)
+		elif x.ry:
+			css["border-radius"] = str(x.ry+stroke/2)
+	
+		#フィルを指定する
+		if "fill" in x.style:
+			css["background-color"] = x.style["fill"]
+			
+		#出力
+		css_style = "".join(["%s:%s;"%style for style in css.items()])
+		self.__css.write("div.%s{%s}\n" % (name, css_style));
+		self.__html.write('<div class="%s"></div>\n' % name);
+		
+	def __del__(self):
+		self.__html.close()
+		self.__css.close()
+
+def main():
+	filename = sys.argv[1]
+	p = svg.Parser()
+	p.parse(open(filename, "r")).callHandler(CSSWriter("out"))
+	return
+
+if __name__=="__main__":
+	main()
