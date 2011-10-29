09e61aa081c2c68942a5a2e90affd389b82ee0da
Ichinose Shogo <ichinose@stn.nagaokaut.ac.jp>
2011-10-29 15:51:40 +0900

    テスト用SVGファイルをまとめてtestディレクトリに移動

---
+++ test/test.bat
@@ -0,0 +1,10 @@
+python ..\svg2css.py rect.svg
+python ..\svg2css.py rect-rotate.svg
+python ..\svg2css.py ellipse.svg
+python ..\svg2css.py ellipse-rotate.svg
+python ..\svg2css.py opacity.svg
+python ..\svg2css.py droid.svg
+python ..\svg2css.py gradient.svg
+python ..\svg2css.py use.svg
+python ..\svg2css.py clip.svg
+python ..\svg2css.py --slide slide.svg
