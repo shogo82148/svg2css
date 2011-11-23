577e16f70b308026a16b2da3318485cdb4d7a80e
Ichinose Shogo <ichinose@stn.nagaokaut.ac.jp>
2011-11-23 12:08:34 +0900

    Linux用テストスクリプトを追加

---
+++ test/test.sh
@@ -0,0 +1,15 @@
+python ../svg2css.py --html rect.html --css rect.css rect.svg
+python ../svg2css.py --html rect-rotate.html --css rect-rotate.css rect-rotate.svg
+python ../svg2css.py --html ellipse.html --css ellipse.css ellipse.svg
+python ../svg2css.py --html ellipse-rotate.html --css ellipse-rotate.css ellipse-rotate.svg
+python ../svg2css.py --html opacity.html --css opacity.css opacity.svg
+python ../svg2css.py --html droid.html --css droid.css droid.svg
+python ../svg2css.py --html gradient.html --css gradient.css gradient.svg
+python ../svg2css.py --html use.html --css use.css use.svg
+python ../svg2css.py --html clip.html --css clip.css clip.svg
+python ../svg2css.py --html text.html --css text.css text.svg
+python ../svg2css.py --html image.html --css image.css image.svg
+python ../svg2css.py --html slide.html --css slide.css --slide slide.svg
+python ../svg2css.py --html text-shadow.html --css text-shadow.css text-shadow.svg
+python ../svg2css.py --html box-shadow.html --css box-shadow.css box-shadow.svg
+python ../svg2css.py --html display.html --css display.css display.svg
